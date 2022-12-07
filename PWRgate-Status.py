90094071a1ebf6daec83b1bc8990ccb9dde2b347
SpudGunMan <kellykeeton@yahoo.com>
2022-12-06 18:29:32 -0800

    Initial Release

---
+++ PWRgate-Status.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# V 1.0 Boilerplate West Mountain Radio EPIC PWRgate Telemetry Script
+from serial import *
+import sys
+
+serialPort = "/dev/ttyACM0" # serial port for linux
+baudRate = 9600 # baud rate for device  (9600 is default)
+ser = Serial(serialPort , baudRate, timeout=1, writeTimeout=0) #ensure non-blocking
+ser.flushInput() #flush input buffer, discarding all its contents
+ser.flushOutput() #flush output buffer, aborting current output and discarding all that is in buffer
+
+# initialise variables
+battery=''
+power_supply=''
+solar_voltage=''
+min_voltage=''
+status=''
+line=''
+parsed_line=''
+
+print("\n\nWest Mountain Radio EPIC PWRgate Reporting Tool\n\n")
+print("Attached to Serial" + ser.name + " Telemetry data:") #check which port was really used
+
+while True:
+
+    if ser.in_waiting> 0:
+        line = ser.readline().decode('utf-8').rstrip() # read serial port and decode from binary array to ASCII
+
+        # check for menu which pops up when device is first connected sometimes
+        if line.__contains__(':'):
+            ser.write('\r'.encode()) # send a carriage return to the device to exit menu
+        elif line.__contains__('?'):
+            ser.write('n\r'.encode()) # send a 'n' and carriage return to the device to select no
+        else: # if not a menu then parse data
+
+            #normal (battery charging, PS connected, solar connected)
+            parsed_line = line.replace(',  ',',').replace('= ','=').split() # split line into a list
+
+            # extract values for use
+            status = (parsed_line[0])
+
+            if status.__contains__('PS'): # handle when PS is off and not connected
+                status = "Battery-Only"
+                power_supply = (parsed_line[2].replace('PS=',''))
+                battery = (parsed_line[3].replace('Bat=',''))
+                solar_voltage = (parsed_line[4].replace('Sol=',''))
+                min_voltage = (parsed_line[5].replace('Min=',''))
+            else: # handle when PS is on and connected
+                power_supply = (parsed_line[1].replace('PS=',''))
+                battery = (parsed_line[2].replace('Bat=',''))
+                solar_voltage = (parsed_line[3].replace('Sol=',''))
+                min_voltage = (parsed_line[4].replace('Min=',''))
+
+    print("Battery " + battery + " Power Supply " + power_supply + " Status " + status + " Solar Voltage " + solar_voltage, end = "\r") # print to console
+
+
+ser.close() # close port
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # EPIC-PWRgate-mods
  West Mountain Radio EPIC PWRgate Mods
+
+
+/dev/ttyACM0 at 9600 baud
+
+on connect to device it will typically catch the letter 's' string which puts you into setup, handles escaping the menu and handles parsing of data for some other funky text output bugs in firmware
+- sol value has extra space
+- battery value has extra space
+- unplug power supply and the output is ugly so we clean that up
+
+$ python3 [PWRgate-Status.py](PWRgate-Status.py)
+
+
+```
+West Mountain Radio EPIC PWRgate Reporting Tool
+
+
+Attached to Serial/dev/ttyACM0 Telemetry data:
+Traceback (most recent call last):y 14.03V Status TrickleSolar Voltage0.04V
+
+
+```
+
+```
+West Mountain Radio EPIC PWRgate R2 1.32
+
+Press S to Review/Edit Charge settings
+
+Because jumper is installed changes will be reset on next power cycle.
+
+Battery:  1-Disable, 2-Gel, 3-AGM, 4-LiFePo4, 5-Other:    <0>: 
+Max charge voltage in Volts:    <14.40>: 
+Max charge current in Amps:    <9.98>: 
+Min charge current in Amps:    <0.25>: 
+Trickle current in Amps:    <0.15>: 
+Trickle voltage in Volts:    <13.55>: 
+Recharge voltage in Volts:    <12.10>: 
+Max charge (minutes):    <1500>: 
+Min charger idle (minutes):    <60>: 
+Min supply voltage for charging in Volts:    <12.99>: 
+Battery Save Mode (Y,N) < N >? 
+```
